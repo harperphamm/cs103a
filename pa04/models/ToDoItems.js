f6d91ea67357e4c8d679000d6e19101455dfce40
harperphamm <124912894+harperphamm@users.noreply.github.com>
2023-05-06 16:24:13 -0400

    Update ToDoItems.js

--- pa04/models/ToDoItems.js
+++ pa04/models/ToDoItems.js
@@ -1,19 +1,15 @@
 
 'use strict';
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-var toDoItemSchema = Schema({
+var toDoItemSchema = Schema( {
   item: String,
   completed: Boolean,
   createdAt: Date,
   priority: Number,
-  description: String,
-  category: String,
-  amount: Number,
-  date: Date,
-  userId: ObjectId
-});
+  userId: {type:ObjectId, ref:'user' }
+} );
 
-module.exports = mongoose.model('ToDoItem', toDoItemSchema);
+module.exports = mongoose.model( 'ToDoItem', toDoItemSchema );
